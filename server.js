343689b074fdc354b51a0e2e14d7d668fb99a696
Adam Magaluk <AdamMagaluK@gmail.com>
2014-11-10 05:33:19 -0500

    Added diagrams and new devices

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var zetta = require('zetta');
 var Buzzer = require('zetta-buzzer-edison-driver');
 var Microphone = require('zetta-microphone-edison-driver');
 var LED = require('./devices/led');
+var Bean = require('zetta-bean-driver');
+var WeMo = require('zetta-wemo-driver');
 
 var app = require('./apps/app');
 
@@ -9,6 +11,8 @@ zetta()
   .use(Buzzer, 3)
   .use(Microphone, 0)
   .use(LED, 13)
+  .use(Bean, 'Bean1')
+  .use(WeMo)
   .use(app)
   .listen(1337, function(){
     console.log('Zetta is running at http://localhost:1337');
